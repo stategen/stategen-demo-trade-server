b229dacf0df7b3e360bce0cc11a007721bb2decf
niaoge <78493244@qq.com>
2018-12-16 01:13:00 +0800

    转换为 async await 准备

--- 7-trade-web-cms/src/main/java/com/mycompany/biz/controller/Bbs_topicController.java
+++ 7-trade-web-cms/src/main/java/com/mycompany/biz/controller/Bbs_topicController.java
@@ -9,6 +9,7 @@ import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.stategen.framework.annotation.ApiConfig;
 import org.stategen.framework.annotation.ApiRequestMappingAutoWithMethodName;
+import org.stategen.framework.annotation.SelectProvidor;
 import org.stategen.framework.annotation.State;
 import org.stategen.framework.enums.DataOpt;
 import org.stategen.framework.lite.AntdPageList;
@@ -17,7 +18,9 @@ import org.stategen.framework.lite.Pagination;
 import org.stategen.framework.util.DatetimeUtil;
 import org.stategen.framework.web.cookie.CookieGroup;
 
+import com.mycompany.biz.domain.Province;
 import com.mycompany.biz.domain.Topic;
+import com.mycompany.biz.enums.TopicType;
 
 import io.swagger.annotations.ApiParam;
 
@@ -30,22 +33,13 @@ public class Bbs_topicController extends TopicControllerBase {
 
     @ApiRequestMappingAutoWithMethodName
     @State(init = true, dataOpt = DataOpt.FULL_REPLACE,genForm=true)
-    public AntdPageList<Topic> getTopicPageList(@ApiParam() String topicId,
+    public AntdPageList<Topic> getTopicPageList(
                                                 @ApiParam()@RequestParam(required =false,name="topicIds") ArrayList<String> topicIds,
-                                                @ApiParam() String authorId,
                                                 @ApiParam()@RequestParam(required =false,name="authorIds") ArrayList<String> authorIds,
-                                                @ApiParam() String topicType,
+                                                @ApiParam() TopicType topicType,
                                                 @ApiParam()@RequestParam(required =false,name="topicTypes") ArrayList<String> topicTypes,
-                                                @ApiParam() String content,
-                                                @ApiParam() String contentLike,
                                                 @ApiParam() String title,
                                                 @ApiParam() String titleLike,
-                                                @ApiParam() Date lastReplyAtMin,
-                                                @ApiParam() Date lastReplyAtMax,
-                                                @ApiParam() Long goodMin,
-                                                @ApiParam() Long goodMax,
-                                                @ApiParam() Long topMin,
-                                                @ApiParam() Long topMax,
                                                 @ApiParam() Long visitCountMin,
                                                 @ApiParam() Long visitCountMax,
                                                 @ApiParam() Date testTimestampMin,
@@ -56,10 +50,12 @@ public class Bbs_topicController extends TopicControllerBase {
                                                 @ApiParam() Date testDateMax,
                                                 @ApiParam() Date testTimeMin,
                                                 @ApiParam() Date testTimeMax,
-                                                @ApiParam() Date createTimeMin,
-                                                @ApiParam() Date createTimeMax,
-                                                @ApiParam() Date updateTimeMin,
-                                                @ApiParam() Date updateTimeMax
+                                                
+                                                @SelectProvidor(Province.class)
+                                                @ApiParam()@RequestParam(required =false,name="provinceIds") ArrayList<String> provinceIds,
+                                                
+                                                @ApiParam()@RequestParam(required =false,name="cityIds") ArrayList<String> cityIds
+                                                
                                                 ,@ApiParam(hidden = true) Topic topic
                                                 ,Pagination pagination
 
@@ -88,7 +84,10 @@ public class Bbs_topicController extends TopicControllerBase {
     public Topic insert(@ApiParam() String topicId, @ApiParam() String authorId, @ApiParam() String topicType, @ApiParam() String content,
                         @ApiParam() String title, @ApiParam() String lastReplyAt, @ApiParam() String good, @ApiParam() String top,
                         @ApiParam() Long visitCount, @ApiParam() String createAt, @ApiParam() Date testTimestamp, @ApiParam() Date testDatetime,
-                        @ApiParam() Date testDate, @ApiParam() Date testTime, @ApiParam(hidden = true) Topic topic) {
+                        @ApiParam() Date testDate, @ApiParam() Date testTime, 
+                        @ApiParam() String provinceId,
+                        @ApiParam() String cityId,
+                        @ApiParam(hidden = true) Topic topic) {
         return this.topicService.insert(topic);
     }
 
