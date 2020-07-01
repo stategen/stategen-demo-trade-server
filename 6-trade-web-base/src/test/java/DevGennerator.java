7db15504c6b81bc97225c86a367e7759e349a79f
niaoge <78493244@qq.com>
2020-07-02 06:03:20 +0800

    spring xsd

--- 6-trade-web-base/src/test/java/DevGennerator.java
+++ 6-trade-web-base/src/test/java/DevGennerator.java
@@ -1,12 +1,11 @@
 import cn.org.rapid_framework.generator.ConsoleGenerator;
 
 /***
-  *  可以代替gen.sh table | dal 命令
- * 
+ *  可以代替gen.sh table | dal 命令
+ *
  */
 public class DevGennerator {
 
-    
     public static void main(String[] args) throws Exception {
       ConsoleGenerator consoleGenerator = new ConsoleGenerator();
       consoleGenerator.generate();
