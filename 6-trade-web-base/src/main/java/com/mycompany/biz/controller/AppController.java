a88c97c219b84f6b47a2941d4902c5d9a86a45c0
niaoge <78493244@qq.com>
2021-01-05 09:21:02 +0800

    Sentinel限流降级截获包装ok

--- 6-trade-web-base/src/main/java/com/mycompany/biz/controller/AppController.java
+++ 6-trade-web-base/src/main/java/com/mycompany/biz/controller/AppController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
@@ -18,13 +19,13 @@ import org.stategen.framework.annotation.Wrap;
 import org.stategen.framework.enums.DataOpt;
 import org.stategen.framework.lite.SimpleResponse;
 import org.stategen.framework.lite.enums.MenuType;
+import org.stategen.framework.util.BusinessAssert;
 import org.stategen.framework.util.CollectionUtil;
 import org.stategen.framework.util.StringUtil;
 import org.stategen.framework.web.cookie.CookieGroup;
-import com.baidu.fsg.uid.impl.CachedUidGenerator;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
-
+import com.baidu.fsg.uid.impl.CachedUidGenerator;
 import com.mycompany.biz.domain.City;
 import com.mycompany.biz.domain.Hoppy;
 import com.mycompany.biz.domain.Menu;
@@ -39,50 +40,46 @@ import com.mycompany.biz.service.ProvinceService;
 import com.mycompany.biz.service.RegionService;
 import com.mycompany.biz.service.UserService;
 
-import ch.qos.logback.classic.joran.ReconfigureOnChangeTask;
 import io.swagger.annotations.ApiParam;
 
 @ApiConfig(menu = false)
 @RequestMapping("/api/app")
 @Wrap
 public class AppController {
+    
     final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(AppController.class);
-
+    
     @Resource
     private CookieGroup<LoginCookieNames> loginCookieGroup;
-	
+    
     @Resource
     private CachedUidGenerator cachedUidGenerator;
-		
+    
     @Resource
     private ProvinceService provinceService;
-
+    
     @Resource
     private CityService cityService;
-
+    
     @Resource
     private HoppyService hoppyService;
-
+    
     @Resource
     private RegionService regionService;
     
-
-	
     @Resource(name = "userService")
     private UserService userService;
-
+    
     @Resource
     private MenuService menuService;
-
-
-
+    
     @ApiRequestMappingAutoWithMethodName(name = "")
     @State(area = User.class)
     public SimpleResponse logout(HttpServletResponse response) {
         loginCookieGroup.expireAllCookies();
         return new SimpleResponse(true, "退出成功");
     }
-
+    
     @ApiRequestMappingAutoWithMethodName(name = "")
     @State(init = true, initCheck = false, dataOpt = DataOpt.FULL_REPLACE)
     public User getCookieUser() {
@@ -90,37 +87,64 @@ public class AppController {
         if (StringUtil.isEmpty(userId)) {
             return null;
         }
-
+        
         User user = this.userService.getUserByUserId(userId);
         if (user == null) {
             return null;
         }
-
+        
         List<Long> visitsIds = this.menuService.getMenusByUserId(user.getUserId(), MenuType.MENU);
         user.setVisitsIds(visitsIds);
         return user;
     }
-
+    
     @ApiRequestMappingAutoWithMethodName(name = "获所所有菜单", method = RequestMethod.GET)
     @State(init = true, initCheck = false, dataOpt = DataOpt.FULL_REPLACE)
     public List<Menu> getAllMenus() {
         return this.menuService.getAllMenus();
     }
-
+    
     @ApiRequestMappingAutoWithMethodName(name = "获取用户")
-    public List<User> getUserOptions(@RequestParam(required = false, name = "userIds") ArrayList<String> userIds){
+    public List<User> getUserOptions(@RequestParam(required = false, name = "userIds") ArrayList<String> userIds) {
         return null;
     }
     
+    /***测试seata分布式事务*/
+    @ApiRequestMappingAutoWithMethodName(method = RequestMethod.GET)
+    public User testSeataAt() {
+        User user = this.userService.appendUserAge("2");
+        return user;
+    }
     
     /***测试seata分布式事务*/
     @ApiRequestMappingAutoWithMethodName(method = RequestMethod.GET)
-    public User  testSeataAt() {
-        User appendUserAge = this.userService.appendUserAge("2");
-        ReconfigureOnChangeTask reconfigureOnChangeTask;
-        return appendUserAge;
+    @SentinelResource(/* blockHandler = "orderBlockHandler",fallback = "orderFallback",*/ )
+    public User testSentinel() {
+        User user = this.userService.getUserByUserId("1");
+        
+        Random rand = new Random();
+        int randInt = rand.nextInt(2)+1;
+        BusinessAssert.mustFalse(2==randInt, "演示降级：随机数不能为2");
+        return user;
     }
-
+    
+    /**
+     * 定义降级逻辑
+     *  hystrix和sentinel
+     *      熔断执行的降级方法
+     *      抛出异常执行的降级方法
+     */
+//    public User orderBlockHandler() {
+//        BusinessAssert.throwException("触发熔断的降级方法");
+//        return null;
+//    }
+//
+//    public User orderFallback() {
+//        BusinessAssert.throwException("抛出异常执行的降级方法");
+//        return null;
+//    }
+//    
+    
     @ApiRequestMappingAutoWithMethodName(method = RequestMethod.GET)
     public String testUid() {
         long uid = this.cachedUidGenerator.getUID();
@@ -130,81 +154,64 @@ public class AppController {
         String parseUID = cachedUidGenerator.parseUID(uid);
         return parseUID;
     }
-
-
-
-
     
     //这是一个dubbo服务
     /*
      * @Resource private UserServiceAuth userServiceAuth;
      * 
-     * @ApiRequestMappingAutoWithMethodName(method = RequestMethod.GET) public
-     * List<Long> getLongs(Long parentRegionId) { List<Long> longParams =new
-     * ArrayList<Long>(Arrays.asList(10L,11L,100000000000000L));
+     * @ApiRequestMappingAutoWithMethodName(method = RequestMethod.GET) public List<Long> getLongs(Long parentRegionId) { List<Long>
+     * longParams =new ArrayList<Long>(Arrays.asList(10L,11L,100000000000000L));
      * 
-     * if (logger.isInfoEnabled()) { logger.info(new
-     * StringBuilder("输出info信息: userServiceAuth!=null:").append(userServiceAuth!=
-     * null).toString()); } List<Long> longs =
-     * userServiceAuth.getLongs(longParams); for (Long long1 : longs) {
+     * if (logger.isInfoEnabled()) { logger.info(new StringBuilder("输出info信息: userServiceAuth!=null:").append(userServiceAuth!=
+     * null).toString()); } List<Long> longs = userServiceAuth.getLongs(longParams); for (Long long1 : longs) {
      * System.out.println("long1<===========>:" + long1); } return longs; }
      */
     /*
-     * @ApiRequestMappingAutoWithMethodName(method = RequestMethod.GET) public
-     * List<Region> testRegions(Long parentRegionId) { User user = new User();
-     * user.setAddress("abc") .setAddressLike("cde") .setAge(10) .setAgeMax(100)
-     * .setAgeMin(10) .setAvatarImgIds(Arrays.asList("abc", "cde"))
-     * .setBirthdayDate(new Date()) .setBirthdayDateMax(new Date())
-     * .setBirthdayDateMin(new Date()) .setCityId("abc")
-     * .setCityIds(Arrays.asList("abc")) .setCreateTime(new Date())
-     * .setCreateTimeMin(new Date()) .setEmail("abc") .setGrade(1L)
-     * .setGradeMax(1L) .setGradeMin(1L) .setPostAddressId(1L)
-     * .setPostAddressIds(Arrays.asList(1L)) .setRoleType(RoleType.ADMIN)
-     * .setWorkTime(new Date()) .setRoleTypes(Arrays.asList(RoleType.ADMIN,
-     * RoleType.DEVELOPER)); this.userService.getPageList(user, 1, 1);
+     * @ApiRequestMappingAutoWithMethodName(method = RequestMethod.GET) public List<Region> testRegions(Long parentRegionId) { User user =
+     * new User(); user.setAddress("abc") .setAddressLike("cde") .setAge(10) .setAgeMax(100) .setAgeMin(10)
+     * .setAvatarImgIds(Arrays.asList("abc", "cde")) .setBirthdayDate(new Date()) .setBirthdayDateMax(new Date()) .setBirthdayDateMin(new
+     * Date()) .setCityId("abc") .setCityIds(Arrays.asList("abc")) .setCreateTime(new Date()) .setCreateTimeMin(new Date()) .setEmail("abc")
+     * .setGrade(1L) .setGradeMax(1L) .setGradeMin(1L) .setPostAddressId(1L) .setPostAddressIds(Arrays.asList(1L))
+     * .setRoleType(RoleType.ADMIN) .setWorkTime(new Date()) .setRoleTypes(Arrays.asList(RoleType.ADMIN, RoleType.DEVELOPER));
+     * this.userService.getPageList(user, 1, 1);
      * 
-     * PageList<Region> pageList = this.regionService.getPageList(new
-     * Region().setCreateTimeMax(new Date()) .setParentRegionId(parentRegionId)
-     * .setParentRegionIds(Arrays.asList(parentRegionId, 257L)), 100, 1); return
-     * pageList.getItems(); }
+     * PageList<Region> pageList = this.regionService.getPageList(new Region().setCreateTimeMax(new Date())
+     * .setParentRegionId(parentRegionId) .setParentRegionIds(Arrays.asList(parentRegionId, 257L)), 100, 1); return pageList.getItems(); }
      */
-	 
+    
     @ApiRequestMappingAutoWithMethodName(name = "省份")
     public List<Province> getProvinceOptions() {
         List<Province> provinceOptions = this.provinceService.getProvinceOptions();
         return provinceOptions;
     }
-
+    
     @ApiRequestMappingAutoWithMethodName(name = "城市")
-    public List<City> getCityOptions(@ApiParam("provinceId")
-    String provinceId) {
+    public List<City> getCityOptions(@ApiParam("provinceId") String provinceId) {
         return this.cityService.getCityOptions(provinceId);
     }
-    
-
     
     @ApiRequestMappingAutoWithMethodName(name = "爱好", method = RequestMethod.GET)
     public List<Hoppy> getHoppyOptions() {
         return this.hoppyService.getHoppyOptions();
     }
-
+    
     @ApiRequestMappingAutoWithMethodName(method = RequestMethod.GET)
     @Wrap(exclude = true)
     @SentinelResource
     public String test() {
         return "test";
     }
-
+    
     @ApiRequestMappingAutoWithMethodName(name = "获取地区")
-    public List<Region> getRegionOptions(@ApiParam("父ID")
-    @RequestParam(required = false, name = "parentRegionIds")   ArrayList<Long> parentRegionIds) {
+    public List<Region> getRegionOptions(
+            @ApiParam("父ID") @RequestParam(required = false, name = "parentRegionIds") ArrayList<Long> parentRegionIds) {
         if (CollectionUtil.isNotEmpty(parentRegionIds)) {
             //客户端不必知道根值或处理根值的问题，直接传null上来
             if (parentRegionIds.get(0) == null) {
                 parentRegionIds.set(0, 0L);
             }
         }
-
+        
         List<Region>            regionOptions      = this.regionService.getRegionOptions(parentRegionIds);
         Map<Long, List<Region>> regionMap          = CollectionUtil.toGroup(regionOptions, Region::getParentRegionId);
         Long                    lastParentRegionId = CollectionUtil.getLast(parentRegionIds);
@@ -218,10 +225,10 @@ public class AppController {
                 } else {
                     children = null;
                 }
-
+                
             }
         }
         return regionOptions;
     }
-
+    
 }
